1920d1ff57357e09a74ac340f34b558b64e05aad
VaishnaviGoud <156517635+VaishnaviGoud407@users.noreply.github.com>
2025-08-21 13:07:42 +0530

    Create Day7.cpp
    Challenge Day 7 on - Trapping rain water

---
+++ Day7.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+       int left=0;
+       int right=n-1;
+       int leftmax=0;
+       int rightmax=0;
+       int water=0;
+       while(left<right){
+        if(height[left]<=height[right]){
+            if(leftmax<=height[left]){
+                leftmax=height[left];
+            }
+            else{
+                water+=leftmax-height[left];
+            }
+            left++;
+        }
+        else{
+            if(rightmax<=height[right]){
+                rightmax=height[right];
+            }
+            else{
+                water+=rightmax-height[right];
+            }
+            right--;
+        }
+       } 
+       return water;
+    }
+};
